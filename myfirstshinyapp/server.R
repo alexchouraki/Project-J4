fdb2da6b24cc235efe5fc34630dd03adfc9f95c9
Alexandre Chouraki <alexchouraki@MacBook-Air-de-Alexandre-1408.local>
2017-10-11 15:21:22 +0200

    coucou

---
+++ myfirstshinyapp/server.R
@@ -0,0 +1,27 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyServer(function(input, output) {
+
+  output$distPlot <- renderPlot({
+
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2]
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+
+    # calculate the average time before two eruptions
+    output$text <- renderPrint({
+      waiting <- mean(x)
+      cat("Il faut attendre en moyenne ", waiting, "entre deux éruptions")})
+    })
+  })
+
